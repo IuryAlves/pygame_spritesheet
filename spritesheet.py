2cb0ab31f1d0288bd4e65673e91f07d6b8322a6d
iuryalves <iuryalves20@gmail.com>
2015-05-21 00:19:49 -0300

    spriteshhit module

---
+++ spritesheet.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+"""
+This module handles sprite sheets
+This was taken https://www.pygame.org/wiki/Spritesheet?parent=CookBook
+"""
+
+import pygame
+
+class Spritesheet(object):
+    def __init__(self, filename):
+        try:
+            self.sheet = pygame.image.load(filename).convert()
+        except pygame.error, message:
+            print 'Unable to load spritesheet image:', filename
+            raise SystemExit, message
+
+    def image_at(self, rectangle, colorkey=None):
+        """
+        Load a specific image from a specific rectangle
+        """
+        rect = pygame.Rect(rectangle)
+        image = pygame.Surface(rect.size).convert()
+        image.blit(self.sheet, (0, 0), rect)
+        if colorkey is not None:
+            if colorkey == -1:
+                colorkey = image.get_at((0,0))
+            image.set_colorkey(colorkey, pygame.RLEACCEL)
+        return image
+
+    def images_at(self, rects, colorkey=None):
+        """
+        Loads multiple images, supply a list of coordinates
+        """
+        return [self.image_at(rect, colorkey) for rect in rects]
+
+    def load_strip(self, rect, image_count, colorkey = None):
+        """
+        Loads a strip of images and returns them as a list
+        """
+        tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
+                for x in range(image_count)]
+        return self.images_at(tups, colorkey)
