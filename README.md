5749a3b2d54617307ccf4d918a9617c59b9ce5b0
iuryalves <iuryalves20@gmail.com>
2015-05-21 00:42:42 -0300

    readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pygame_spritesheets
 Handling spritesheets in pygame in a easy way.
+
+### Here is a quick example on how you would use this
+
+
+	import spritesheet
+	...
+	ss = spritesheet.spriteshee('somespritesheet.png')
+	# Sprite is 16x16 pixels at location 0,0 in the file...
+	image = ss.image_at((0, 0, 16, 16))
+	images = []
+	# Load two images into an array, their transparent bit is (255, 255, 255)
+	images = ss.images_at((0, 0, 16, 16),(17, 0, 16,16), colorkey=(255, 255, 255))
--- spritesheet.py
+++ spritesheet.py
@@ -7,7 +7,9 @@ This was taken https://www.pygame.org/wiki/Spritesheet?parent=CookBook
 
 import pygame
 
+
 class Spritesheet(object):
+
     def __init__(self, filename):
         try:
             self.sheet = pygame.image.load(filename).convert()
@@ -24,7 +26,7 @@ class Spritesheet(object):
         image.blit(self.sheet, (0, 0), rect)
         if colorkey is not None:
             if colorkey == -1:
-                colorkey = image.get_at((0,0))
+                colorkey = image.get_at((0, 0))
             image.set_colorkey(colorkey, pygame.RLEACCEL)
         return image
 
@@ -34,10 +36,10 @@ class Spritesheet(object):
         """
         return [self.image_at(rect, colorkey) for rect in rects]
 
-    def load_strip(self, rect, image_count, colorkey = None):
+    def load_strip(self, rect, image_count, colorkey=None):
         """
         Loads a strip of images and returns them as a list
         """
-        tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
+        tups = [(rect[0] + rect[2] * x, rect[1], rect[2], rect[3])
                 for x in range(image_count)]
         return self.images_at(tups, colorkey)
