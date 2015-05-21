63c2ffb26f44a73650d4e0cc29aca653f80186a1
iuryalves <iuryalves20@gmail.com>
2015-05-21 00:55:15 -0300

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+from distutils.core import setup
+
+setup(name='Pygame SpriteSheets',
+      version='1.0',
+      description='Handling spritesheets in pygame in a easy way.',
+      author='unknown',
+      py_modules=['spritesheet']
+      )
